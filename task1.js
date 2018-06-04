e627742e21c35ec81c8ec99ca74e1f4671f4622f
AndrewLoginov <andrew.loginov13@gmail.com>
2018-06-04 18:48:29 +0500

    gb12

--- task1.js
+++ task1.js
@@ -1,43 +1,127 @@
-var i = process.argv[2];    
-var a = +process.argv[3];   
-var b = +process.argv[4];   
+function onlyDigits(vf)
+{
 
-if (Math.min(a,b)>=2 & Math.max(a,b)<=36)  
+    if (vf < 11)
+        return 1;
+    else
+        return 0;
+}
+function maxSymbol(vff)
 {
-    var rn = 0;
-    for (var n=0; n<i.length; n++)
+    if (vff < 11)
+        return 47 + vff;
+    else
+        return 54 + vff;
+}
+function wrongArgs(iV, fV, bV)
+{
+    if (iV.length > 5)
+        return 'Недопустимое число';
+    else
+        if ((iV.length === 5) && (iV !== '10000'))
+            return 'Недопустимое число';
+    if ((isNaN(fV)) || (isNaN(bV)))
+        return 'Недопустимое основание системы счисления';
+    else
     {
-        var _i = i.charCodeAt(n)-48; 
-        if (_i>9) _i = _i-39;        
-        if (_i>=a){
-            console.log("чило i не соответсвует исходной системе счисления")
-            return;
-        }
-        rn = rn*a + _i;
+        fV = parseInt(fV);
+        bV = parseInt(bV);
     }
-    console.log(rn);
-     
-    if (rn>=0 & rn<=10000){
-        
-        var out = "";
-        while (rn>0){
-            var _o = (rn%b)+48;  
-            if (_o>57) _o=_o+39; 
-            out = String.fromCharCode(_o)+out;
-            rn  = Math.trunc(rn/b) ;
+    if ((fV < 2) || (fV > 36) || (bV < 2) || (bV > 36))
+        return 'Недопустимое основание системы счисления';
+    const max = maxSymbol(fV);
+    let symbol;
+    if (onlyDigits(fV) === 1)
+    {       
+        for (let count = 0; count < iV.length; count++)
+        {
+            symbol = iV.charCodeAt(count);          
+            if ((symbol < 48) || (symbol > max))
+                return 'Недопустимый символ в числе';
         }
-        console.log(out);
-    } else {
-        console.log("0 <= i >= 10000 - не соблюдается");
     }
-} else                       
+    else
+    {
+        for (lrt count = 0; count < iV.length; count++)
+        {
+            symbol = iV.charCodeAt(count);
+            if ((symbol < 48) || ((symbol > 57) && (symbol < 65)) || ((symbol > max) && (symbol < 97)) || (symbol > (max + 32)))
+                return 'Недопустимый символ в числе';
+        }
+    }
+    return 0;
+}
+function parseToSystem10(ival)
 {
-  if (a<2 | a>36)            
-  {
-    console.log('2<= a <= 36 - не соблюдается');
-  }
-  if (b<2 | b>36)            
-  {
-    console.log('2<= b <= 36 - не соблюдается'); 
-  }
+    if (isNaN(ival))
+    {
+        var symbolCode = ival.charCodeAt(0);
+        if (symbolCode > 96)
+            return symbolCode - 87;
+        else
+            return symbolCode - 55;
+    }
+    return parseInt(iVal);
+}
+var i = process.argv[2];
+var f = process.argv[3];
+var b = process.argv[4];
+var checkArguments = wrongArgs(i, f, b);
+if (checkArguments !== 0)
+{
+    process.stdout.write(checkArguments);
+}
+else
+{
+    f = parseInt(f);
+    b = parseInt(b);
+    if (f === b)
+    {
+        process.stdout.write(i);
+    }
+    else
+    {
+        let system10 = 0;
+        let systemB = '';
+        let fullNumber;
+        let output = '';
+        const symbols = '0123456789abcdefghijklmnopqrstuvwxyz';
+        if (f < 10)
+        {
+            for (let count = 0; count < i.length; count++)
+            {
+                system10 += parseInt(i[count])*(Math.pow(f,(i.length - count - 1)));
+            }
+        }
+        else
+            if (f !== 10)
+            {
+                for (let count = 0; count < i.length; count++)
+                {
+                    system10 += parseToSystem10(i[count])*(Math.pow(f,(i.length - count - 1)));
+                }
+            }
+            else
+            {
+                system10 = parseInt(i);
+            }   
+
+        if (b !== 10)
+        {
+            do
+            {
+                fullNumber = symbols[system10 % b];
+                systemB += fullNumber;
+                system10 = Math.floor(system10 / b);            
+            }
+            while (system10 !== 0);
+            for (let count = systemB.length - 1; count >= 0; count--)
+            {
+                output += systemB[count];
+            }
+        }
+        else
+            output = system10;
+        process.stdout.write(String(output));
+    }
 }
